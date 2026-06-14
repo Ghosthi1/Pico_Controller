30510fd11814395e3f0235861f3d5da3165b71d3
ghosthi <c-forsyth02@sky.com>
2026-06-14 18:22:29 +0100

    flashing led

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use panic_halt as _;
 use rp235x_hal as hal;
+use hal::clocks::Clock;
+use embedded_hal::digital::OutputPin;
 
 #[unsafe(link_section = ".start_block")]
 #[used]
@@ -10,7 +12,28 @@ pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
 
 #[hal::entry]
 fn main() -> ! {
-    let _pac = hal::pac::Peripherals::take().unwrap();
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+    let core = cortex_m::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        12_000_000u32,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    ).ok().unwrap();
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
+    let mut led_pin = pins.gpio0.into_push_pull_output();
 
-    loop {}
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+
 }
