6c1702c7190c1f3b17b0c4b24ec5eb42a9d74d9e
ghosthi <c-forsyth02@sky.com>
2026-06-16 22:45:13 +0100

    start usb

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::delay;
 use panic_halt as _;
 use rp235x_hal as hal;
-use hal::clocks::Clock;
 use embedded_hal::digital::{InputPin, OutputPin};
+use usb_device::bus::UsbBusAllocator;
+
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
 
 #[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
 
+use usbd_hid::descriptor::generator_prelude::*;
+
+#[gen_hid_descriptor(
+      (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = GAMEPAD) = {
+          (usage_page = BUTTON, usage_min = BUTTON_1, usage_max = BUTTON_4) = {
+              #[packed_bits = 4] #[item_settings(data,variable,absolute)] buttons=input;
+          };
+          (usage_page = GENERIC_DESKTOP,) = {
+              #[packed_bits = 4] #[item_settings(constant,variable,absolute)]
+  padding=input;
+          };
+      }
+)]
+
+#[allow(dead_code)]
+struct GamepadReport {
+    buttons: u8,
+    padding: u8,
+}
+
 #[hal::entry]
 fn main() -> ! {
     let mut pac = hal::pac::Peripherals::take().unwrap();
-    let core = cortex_m::Peripherals::take().unwrap();
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-    let clocks = hal::clocks::init_clocks_and_plls(
-        12_000_000u32,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    ).ok().unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let sio = hal::Sio::new(pac.SIO);
     let pins = hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
 
